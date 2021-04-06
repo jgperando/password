2f93eee50be8bb4bcc5524d3d20bbedebf8c58f1
jgperando <77844715+jgperando@users.noreply.github.com>
2021-04-06 11:11:31 -0400

    commit on main

--- password.py
+++ password.py
@@ -3,7 +3,7 @@ import random
 uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lowercase_letters = uppercase_letters.lower()
 digits = "0123456789"
-symbols = "()[]{},;:.-_/\\?+*# "
+symbols = "()[],;:.-_/&?+*#^% "
 
 upper, lower, nums, syms = True, True, True, True
 
